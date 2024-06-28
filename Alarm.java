a5e4cfd5cad477c47a56f3ebc31cdbfa2f3d82e0
Dishan Anuradha <dishananuradha@gmail.com>
2024-06-29 01:53:07 +0530

    Display.java created

--- Alarm.java
+++ Alarm.java
@@ -4,11 +4,12 @@ import java.awt.*;
 class Alarm extends Device{
     private JLabel lblAlarm;
 	
-	Alarm(){
+	public Alarm(){
 		lblAlarm=new JLabel("  ");
 		lblAlarm.setFont(new Font("",1,25));
 		add(lblAlarm);
 	}
+
 	public void update(int waterLevel){
 		lblAlarm.setText(waterLevel>=50 ? "Alarm ON":"Alarm OFF");
 	}
--- Display.java
+++ Display.java
@@ -1,3 +1,16 @@
-class Display {
+import javax.swing.*;
+import java.awt.*;
+
+class Display extends Device{
+    private JLabel lblDisplay;
+	
+	public Display(){
+		lblDisplay=new JLabel("  ");
+		lblDisplay.setFont(new Font("",1,25));
+		add(lblDisplay);
+	}
     
+	public void update(int waterLevel){
+		lblDisplay.setText(String.valueOf(waterLevel));
+	}
 }
