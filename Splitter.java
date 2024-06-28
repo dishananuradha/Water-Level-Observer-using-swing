0ff67003cc5799c8d4f84f48e9c2f3dfacbf9bd8
Dishan Anuradha <dishananuradha@gmail.com>
2024-06-29 01:39:29 +0530

    Splitter.java created

--- Splitter.java
+++ Splitter.java
@@ -1,3 +1,17 @@
-class Splitter {
-    
+import javax.swing.*;
+import java.awt.*;
+
+class Splitter extends Device{
+    private JLabel lblSplitter;
+	
+	public Splitter(){
+		lblSplitter=new JLabel("  ");
+		lblSplitter.setFont(new Font("",1,25));
+		add(lblSplitter);
+		
+	}
+
+	public void update(int waterLevel){
+		lblSplitter.setText(waterLevel>=75 ? "Splitter ON":"Splitter OFF");
+	}
 }
