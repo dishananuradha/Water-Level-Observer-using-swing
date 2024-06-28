a01b0ccd096687a42389aa9b299a88b4c1ebcc58
Dishan Anuradha <dishananuradha@gmail.com>
2024-06-29 01:11:38 +0530

    Upload files

---
+++ WaterTank.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.*;
+
+class WaterTank extends JFrame{
+    private static WaterTank waterTank;
+    private ControlRoom controlRoom;
+    private JSlider waterLevelSlider;
+
+    private WaterTank(){
+        controlRoom = new ControlRoom();
+        setSize(300,300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setLayout(new FlowLayout());
+
+        waterLevelSlider=new JSlider(JSlider.VERTICAL,0,100,50);
+		waterLevelSlider.addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent evt){
+				controlRoom.setWaterLevel(waterLevelSlider.getValue());
+			}
+		});
+		add(waterLevelSlider);
+    }
+
+    public static WaterTank getInstance(){
+        if(waterTank==null){
+            waterTank=new WaterTank();
+        }
+        return waterTank;
+    }
+}
