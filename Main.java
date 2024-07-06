7e54c4ec9a723c7c3841f290c961b7982d891e57
Dishan Anuradha <dishananuradha@gmail.com>
2024-07-06 23:24:44 +0530

    Main.java edited

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 class Main{
-    public static WaterTank waterTank = WaterTank.getInstance();
-    public static ControlRoom controlRoom = waterTank.getControlRoom();
+    private static WaterTank waterTank = WaterTank.getInstance();
     public static void main(String[] args){
         waterTank.setVisible(true);
+        addDevices();
+    }
+
+    public static void addDevices(){
+        ControlRoom controlRoom = waterTank.getControlRoom();
         controlRoom.addDevice(new Display(waterTank));
         controlRoom.addDevice(new Alarm(waterTank));
         controlRoom.addDevice(new Splitter(waterTank));
