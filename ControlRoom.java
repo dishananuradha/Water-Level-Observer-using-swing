19b314b0fac9cf187b34b3f78f3afa7e35ef19fa
Dishan Anuradha <dishananuradha@gmail.com>
2024-07-07 00:54:12 +0530

    ControlRoom.java edited

--- ControlRoom.java
+++ ControlRoom.java
@@ -2,21 +2,13 @@ import java.util.ArrayList;
 
 class ControlRoom{
     private ArrayList<DeviceInterface>deviceList=new ArrayList<>();
-	private int waterLevel;
 
 	public void addDevice(DeviceInterface device){
 		deviceList.add(device);
 	}
 
 	public void setWaterLevel(int waterLevel){
-		if(this.waterLevel!=waterLevel){
-			this.waterLevel=waterLevel;
-			notifyDevices();
-		}
-	}
-
-	public void notifyDevices(){
-		for(DeviceInterface device : deviceList){
+        for(DeviceInterface device : deviceList){
 			device.update(waterLevel);
 		}
 	}
